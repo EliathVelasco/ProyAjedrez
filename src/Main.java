9addb4eda3da6a41cceb0016d74bfc66be95e20b
Eliath <eliath.velasco@epn.edu.ec>
2022-07-19 15:52:28 -0500

    Cambio definitivo

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,7 @@ import Partida.Partida;
 
 public class Main {
     public static void main(String[] args) {
-
         Partida partida = new Partida();
-        System.out.println("ELIATH PARTE DE LA COMUNIDAD LGBTQQQ+");
         partida.preguntarJugada();
-
     }
-    //Mov
 }
