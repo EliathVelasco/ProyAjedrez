17215445ae94dd3f0106a7bf4e8b05ae34b44528
J34nPC24 <jean.cotera@epn.edu.ec>
2022-07-19 15:51:07 -0500

    La puta DE TU MADRE

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Partida partida = new Partida();
-        System.out.println("eeee");
+        System.out.println("ELIATH PARTE DE LA COMUNIDAD LGBTQQQ+");
         partida.preguntarJugada();
 
     }
