cc467ab875fe3c2f1058380cc3a7ca317d533490
Eliath <eliath.velasco@epn.edu.ec>
2022-07-29 10:26:30 -0500

    Agregar clase Jugada

---
+++ src/Partida/Jugada.java
@@ -0,0 +1,66 @@
+package Partida;
+
+import Errores.MovimientoNoVálido;
+import Errores.SintáxisInválida;
+
+public class Jugada {
+    private int[] coordenadasIniciales = new int[2];
+    private int[] coordenadasFinales = new int[2];
+    private ColorPiezas color;
+    public Jugada(String jugada, ColorPiezas colorPiezas) throws MovimientoNoVálido, SintáxisInválida {
+        validarFilas(jugada);
+
+        coordenadasIniciales[1] = transformarColumnaIngresadaEnIndice(jugada.charAt(0));
+        coordenadasIniciales[0] = Character.getNumericValue(jugada.charAt(1))-1;
+
+        coordenadasFinales[1] = transformarColumnaIngresadaEnIndice(jugada.charAt(3));
+        coordenadasFinales[0] = Character.getNumericValue(jugada.charAt(3))-1;
+
+        color = colorPiezas;
+    }
+    private void validarFilas(String jugada) throws SintáxisInválida, MovimientoNoVálido {
+        if (jugada.length() > 4){
+            throw new SintáxisInválida("Hay demasiados datos en la jugada");
+        }
+        if (jugada.length() < 4){
+            throw new SintáxisInválida("Faltan datos en la jugada");
+        }
+        if (Character.getNumericValue(jugada.charAt(1)) >= 0 && Character.getNumericValue(jugada.charAt(1)) <=Tablero.NUMERO_DE_FILAS){
+            throw new MovimientoNoVálido("La fila "+ jugada.charAt(1) + " no existe");
+        }
+        if (Character.getNumericValue(jugada.charAt(3)) >= 0 && Character.getNumericValue(jugada.charAt(3)) <= Tablero.NUMERO_DE_FILAS){
+            throw new MovimientoNoVálido("La fila "+ jugada.charAt(3) + " no existe");
+        }
+    }
+    private int transformarColumnaIngresadaEnIndice(char letra) throws SintáxisInválida {
+
+        String cadenaDeLetras = "abcdefghijklmnopqrstuvwxyz";
+
+        for (int i = 0; i < Tablero.NUMERO_DE_COLUMNAS; i++) {
+            if (cadenaDeLetras.charAt(i) == letra) {
+                return  i;
+            }
+        }
+        throw new SintáxisInválida("La columna " + letra + " no existe");
+    }
+
+    public int getFilaInicial() {
+        return coordenadasIniciales[0];
+    }
+
+    public int getColumnaInicial() {
+        return coordenadasIniciales[1];
+    }
+
+    public ColorPiezas getColor() {
+        return color;
+    }
+
+    public int getFilaFinal() {
+        return coordenadasFinales[0];
+    }
+
+    public int getColumnaFinal() {
+        return coordenadasFinales[1];
+    }
+}
--- src/Partida/Jugador.java
+++ src/Partida/Jugador.java
@@ -1,6 +1,8 @@
 package Partida;
 import Errores.*;
 
+import java.util.Scanner;
+
 public class Jugador {
 
     private ColorPiezas colorPiezas;
@@ -9,35 +11,10 @@ public class Jugador {
 
     }
 
-    public void ingresarJugada(String casillas, Tablero tablero) throws MovimientoNoVálido, SintáxisInválida {
-
-        if (casillas.length() !=4){
-            throw new SintáxisInválida("Coordenadas incompletas");
-        }
-
-        int[] columna = new int[2];
-        int[] fila = new int[2];
-
-        columna[0] = transformarColumnaIngresadaEnIndice(casillas.charAt(0));
-        columna[1] = transformarColumnaIngresadaEnIndice(casillas.charAt(2));
-
-        fila[0] = Character.getNumericValue(casillas.charAt(1))-1;
-        fila[1] = Character.getNumericValue(casillas.charAt(3))-1;
-
-        tablero.actualizarTablero(columna, fila, colorPiezas);
-        
+    public Jugada ingresarJugada() throws MovimientoNoVálido, SintáxisInválida, FilaInvalida {
+        Scanner scannerDelBlanco = new Scanner(System.in);
+        String jugadaDelBlanco = scannerDelBlanco.next();
+        Jugada jugada = new Jugada(jugadaDelBlanco, colorPiezas);
+        return jugada;
     }
-
-    private int transformarColumnaIngresadaEnIndice(char letra) throws SintáxisInválida {
-
-        String cadenaDeLetras = "abcdefgh";
-
-        for (int i = 0; i < 8; i++) {
-            if (cadenaDeLetras.charAt(i) == letra) {
-                return  i;
-            }
-        }
-        throw new SintáxisInválida("Ingreso no válido.");
-    }
-
 }
